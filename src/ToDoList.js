cdd3675bde91311e80d7dd560555ab37c1e43282
bonbo0811 <abcdefg999gp811@gmail.com>
2023-04-15 16:01:51 +0900

    4/15

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -5,11 +5,17 @@ const ToDoList = () => {
     <div>
         <nav class="navbar navbar-light bg-light">
             <div class="container-fluid">
-                <span class="navbar-brand mb-0 p-3 h1 m-auto">ToDoリスト
-                    <button class="btn btn-primary ms-5" type="submit">登録</button>
-                </span>
+                <span class="navbar-brand p-3 h1 m-auto ps-5">ToDoリスト</span>
+                <button class="btn btn-primary me-5" type="submit">登録</button>
             </div>
         </nav>
+        <ul class="text-center list-unstyled ">
+            <li class="col-md-8 border p-2 m-4">ToDo1</li>
+            <li class="col-md-8 border p-2 m-4">ToDo2</li>
+            <li class="col-md-8 border p-2 m-4">ToDo3</li>
+            <li class="col-md-8 border p-2 m-4">ToDo4</li>
+            <li class="col-md-8 border p-2 m-4">ToDo5</li>
+        </ul>
     </div>
     )
 }
