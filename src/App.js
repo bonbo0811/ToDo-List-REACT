358c72e23c8ea982d9c3022642961fefdf6f4081
bonbo0811 <abcdefg999gp811@gmail.com>
2024-07-13 15:40:06 +0900

    コメント追加

--- src/App.js
+++ src/App.js
@@ -11,10 +11,13 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<ToDoList />} />
+          {/* 一覧表示ページへ遷移 */}
           <Route path="/Register" element={<Register />} />
+          {/* 新規登録ページへ遷移 */}
           <Route path="/Edit/id/:id" element={<TodoEdit />} />
+          {/* Todo編集ページへ遷移 */}
           <Route path="/Delete/id/:id" element={<TodoDelete />} />
-          <Route path="*" element={<NoMatch message="Hello Contact" />} />
+          {/* Todo削除ページへ遷移 */}
       </Routes>
       </div>
   );
